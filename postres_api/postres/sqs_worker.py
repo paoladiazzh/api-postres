d4f354e0e3fcac12cb923f99922f37a38d51bed9
Paola <paola.diaz@cetysuniversidad.edu.mx>
2025-05-26 17:25:30 -0700

    Updated credentials

--- postres_api/postres/sqs_worker.py
+++ postres_api/postres/sqs_worker.py
@@ -6,9 +6,9 @@ def receive_messages():
     sqs = boto3.client(
         'sqs',
         region_name='us-east-1',
-        aws_access_key_id='ASIARDSSQLQUQNBX4IWX',
-        aws_secret_access_key='7da/AeNtTIJJbj6meT+81XxzCHPvbbc+3QgqWHJ+',
-        aws_session_token='IQoJb3JpZ2luX2VjEC0aCXVzLXdlc3QtMiJHMEUCIQDqq1Mf9hXF+Cf1pqeUkOAyyAkOWDQgmabViqowxnUNhgIgdc6km+U2ZFIYEYxGvj8fhQbucoVo8pBQd476Bcu9uUYqrQII5f//////////ARABGgwwNzY0MDg3MDgxMzciDGT25fzxtep1ge46niqBAmGdKHTU/i4lYj2V2SdYG46VVnYxPjipk+yaOAFNrAi6N8m3BemZc5MO4gqmwBe7pWV1yimgl48R57rnjv+pZa76r6lxj3fo2c45dra9EMelRTivqLreavGDSoplklriPXo516DbfRuFLRoh5WUa7ZQAaFlYN+N4mKbDuxGF6ADTJwbsXF7A6lx1DkVLIOUWplQa1owkDK9AKSJOuhms1aqNPbfUOED4HZQmF/R891eU8X33oWwhofe6tKkaPbPgDL/ewq3hDMmezd2uD8Cdqd8hL+zhe+TOwJAPEyHcvbds4SdkuZj5t0mAOCtFMOpEy8ryK1lK7xRNA4aLTEaz7371MNzvv8EGOp0B3IkPnY/HlEiEzJMbHloT3MxoW5f7mo7F/1bpwXHH6V29D9qm+eMz/aXb2rXFqUQonVz+SVN+DslUENgyVFW1kiIiwZ9J61xw7f2SvEoWOHPJsJ0gyEdkp5X6kkI9aNGHirSsxcNnLw/m/Yn41PvtFFzuiiAhTg+4KDNKXgVyZriJ70HsxfZRo42HoXY9CeaveOHysFM5IjEI6FoTAQ=='
+        aws_access_key_id='ASIARDSSQLQU7QAVYKR2',
+        aws_secret_access_key='HYg9DG+anAMYGypUhQc610MQefKXoiTq3qCsaVNm',
+        aws_session_token='IQoJb3JpZ2luX2VjEIj//////////wEaCXVzLXdlc3QtMiJIMEYCIQDskuC91imJ/xhzAljHRU0JR6It52554vA7wNoFKevEVwIhAPcYU9Dk/2NAI9iwkxvYJ6oHFOcCweXvXbqBMNU628/mKqQCCFEQARoMMDc2NDA4NzA4MTM3IgxiksvphppGORcK0QIqgQKeG3EbN1CsWg3uGUA5uI3IQjgpK8qXzYxc8U7Q8K201f7pTeavDH6N/8dxCk5CVJrpyNJAuaEBX7Nh5feqN99nKXMtWHWQ56n8Yh+7o9WHxpzrN1UBSUgbWnV4DfvOuFEK9DWOk0D8UWKbDIvvgHOAtG2hx8LX8OrmJOPYVikoHy+7/qeRkDDT7Hvj5gQik1c6qDPMIFzS5U/QWiw8HwE/JAaP61Y2UpZYBuNx5qPy1YY8V5eVruRvPEFvjwQZbEqifKosiJyIdnPnXOHIxc9OontGhCfGyyOc/gaP6c+2WomkAhLlJmW3XA/WeRV1sjnPMfvrto8tyylHfB+MKNaW9zDbh9TBBjqcAZz85Cv5uvzk/VnP5TGZNVeMlf2WQTq2qBUkr1n5g3tNmNniPq7wsXu4AK1LWlynmac7F7Mb39US1hoMEJyAwTYmLqREorBl6KlRmUlHo7R6IJM1Y3l7qTDydZiKvl7jgxy8H5s/52ANh1Rr3CFpW7sIf1FXQjmxWZ8CGIzEH7CAUX8mnu6phhqS7xJgd5aJd/LYgGUsuLRFrmd7Tw=='
     )
     queue_url = 'https://sqs.us-east-1.amazonaws.com/076408708137/postres-queue'
     
