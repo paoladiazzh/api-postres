3c1640fcb1e0d50bf1f9f9728f19cda349a98930
Paola <paola.diaz@cetysuniversidad.edu.mx>
2025-06-04 22:46:09 -0700

    jiji hola profe

--- postres_api/postres/sqs_worker.py
+++ postres_api/postres/sqs_worker.py
@@ -1,19 +1,24 @@
 import boto3
 import json
+from django.conf import settings
 from postres.views import postres  # lista en memoria
 
 def receive_messages():
-    sqs = boto3.client(
-        'sqs',
-        region_name='us-east-1',
-        aws_access_key_id='ASIARDSSQLQU7QAVYKR2',
-        aws_secret_access_key='HYg9DG+anAMYGypUhQc610MQefKXoiTq3qCsaVNm',
-        aws_session_token='IQoJb3JpZ2luX2VjEIj//////////wEaCXVzLXdlc3QtMiJIMEYCIQDskuC91imJ/xhzAljHRU0JR6It52554vA7wNoFKevEVwIhAPcYU9Dk/2NAI9iwkxvYJ6oHFOcCweXvXbqBMNU628/mKqQCCFEQARoMMDc2NDA4NzA4MTM3IgxiksvphppGORcK0QIqgQKeG3EbN1CsWg3uGUA5uI3IQjgpK8qXzYxc8U7Q8K201f7pTeavDH6N/8dxCk5CVJrpyNJAuaEBX7Nh5feqN99nKXMtWHWQ56n8Yh+7o9WHxpzrN1UBSUgbWnV4DfvOuFEK9DWOk0D8UWKbDIvvgHOAtG2hx8LX8OrmJOPYVikoHy+7/qeRkDDT7Hvj5gQik1c6qDPMIFzS5U/QWiw8HwE/JAaP61Y2UpZYBuNx5qPy1YY8V5eVruRvPEFvjwQZbEqifKosiJyIdnPnXOHIxc9OontGhCfGyyOc/gaP6c+2WomkAhLlJmW3XA/WeRV1sjnPMfvrto8tyylHfB+MKNaW9zDbh9TBBjqcAZz85Cv5uvzk/VnP5TGZNVeMlf2WQTq2qBUkr1n5g3tNmNniPq7wsXu4AK1LWlynmac7F7Mb39US1hoMEJyAwTYmLqREorBl6KlRmUlHo7R6IJM1Y3l7qTDydZiKvl7jgxy8H5s/52ANh1Rr3CFpW7sIf1FXQjmxWZ8CGIzEH7CAUX8mnu6phhqS7xJgd5aJd/LYgGUsuLRFrmd7Tw=='
-    )
-    queue_url = 'https://sqs.us-east-1.amazonaws.com/076408708137/postres-queue'
+    try:
+        sqs = boto3.client(
+            'sqs',
+            region_name=settings.AWS_REGION,
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            aws_session_token=settings.AWS_SESSION_TOKEN
+        )
+        queue_url = 'https://sqs.us-east-1.amazonaws.com/076408708137/postres-queue'
+        
+        response = sqs.receive_message(QueueUrl=queue_url, MaxNumberOfMessages=1)
+        for msg in response.get('Messages', []):
+            body = json.loads(msg['Body'])
+            postres.append(body)
+            sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=msg['ReceiptHandle'])
     
-    response = sqs.receive_message(QueueUrl=queue_url, MaxNumberOfMessages=1)
-    for msg in response.get('Messages', []):
-        body = json.loads(msg['Body'])
-        postres.append(body)
-        sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=msg['ReceiptHandle'])
+    except Exception as e:
+        print(f"Error al recibir mensajes de SQS: {e}")
